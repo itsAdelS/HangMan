f59942c09a2a7b7bd03a18881053325a942e006d
Adel Sliman <115944408+itsAdelS@users.noreply.github.com>
2022-10-18 19:53:20 -0400

    Add files via upload

---
+++ HangMan-Adel Sliman/HangMan- Adel Sliman/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace HangMan__Adel_Sliman
+{
+    partial class HangMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HangMan));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblMissed = new System.Windows.Forms.Label();
+            this.lblWordLength = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.txtLetter = new System.Windows.Forms.TextBox();
+            this.btnWord = new System.Windows.Forms.Button();
+            this.btnLetter = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Location = new System.Drawing.Point(432, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(155, 238);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(149, 219);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblMissed);
+            this.groupBox2.Controls.Add(this.lblWordLength);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(414, 145);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // lblMissed
+            // 
+            this.lblMissed.AutoSize = true;
+            this.lblMissed.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMissed.Location = new System.Drawing.Point(6, 127);
+            this.lblMissed.Name = "lblMissed";
+            this.lblMissed.Size = new System.Drawing.Size(53, 15);
+            this.lblMissed.TabIndex = 1;
+            this.lblMissed.Text = "Missed: ";
+            // 
+            // lblWordLength
+            // 
+            this.lblWordLength.AutoSize = true;
+            this.lblWordLength.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWordLength.Location = new System.Drawing.Point(310, 127);
+            this.lblWordLength.Name = "lblWordLength";
+            this.lblWordLength.Size = new System.Drawing.Size(86, 15);
+            this.lblWordLength.TabIndex = 0;
+            this.lblWordLength.Text = "Word Length: ";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnClose);
+            this.groupBox3.Controls.Add(this.btnNewGame);
+            this.groupBox3.Controls.Add(this.btnClear);
+            this.groupBox3.Controls.Add(this.txtWord);
+            this.groupBox3.Controls.Add(this.txtLetter);
+            this.groupBox3.Controls.Add(this.btnWord);
+            this.groupBox3.Controls.Add(this.btnLetter);
+            this.groupBox3.Location = new System.Drawing.Point(12, 163);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(414, 87);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnClose.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(292, 45);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(116, 36);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "Exit";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnNewGame.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.Location = new System.Drawing.Point(165, 47);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(120, 36);
+            this.btnNewGame.TabIndex = 5;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(6, 46);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(153, 36);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtWord
+            // 
+            this.txtWord.Location = new System.Drawing.Point(292, 19);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(116, 20);
+            this.txtWord.TabIndex = 3;
+            this.txtWord.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtLetter
+            // 
+            this.txtLetter.Location = new System.Drawing.Point(132, 19);
+            this.txtLetter.MaxLength = 1;
+            this.txtLetter.Name = "txtLetter";
+            this.txtLetter.Size = new System.Drawing.Size(27, 20);
+            this.txtLetter.TabIndex = 2;
+            this.txtLetter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnWord
+            // 
+            this.btnWord.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnWord.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWord.Location = new System.Drawing.Point(165, 17);
+            this.btnWord.Name = "btnWord";
+            this.btnWord.Size = new System.Drawing.Size(120, 23);
+            this.btnWord.TabIndex = 1;
+            this.btnWord.Text = "Enter Word!";
+            this.btnWord.UseVisualStyleBackColor = false;
+            this.btnWord.Click += new System.EventHandler(this.btnWord_Click);
+            // 
+            // btnLetter
+            // 
+            this.btnLetter.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnLetter.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLetter.Location = new System.Drawing.Point(6, 16);
+            this.btnLetter.Name = "btnLetter";
+            this.btnLetter.Size = new System.Drawing.Size(120, 23);
+            this.btnLetter.TabIndex = 0;
+            this.btnLetter.Text = "Enter Letter";
+            this.btnLetter.UseVisualStyleBackColor = false;
+            this.btnLetter.Click += new System.EventHandler(this.btnLetter_Click);
+            // 
+            // HangMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(591, 258);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "HangMan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HangMan-Adel Sliman";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblWordLength;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.TextBox txtLetter;
+        private System.Windows.Forms.Button btnWord;
+        private System.Windows.Forms.Button btnLetter;
+        private System.Windows.Forms.Label lblMissed;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ HangMan-Adel Sliman/HangMan- Adel Sliman/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+// Author: Adel Sliman
+//ID: 572643
+//Date: 12/05/2015
+//Purpose: the purpose for this application is so the user can enjoy a nice, classic game of Hangman.
+namespace HangMan__Adel_Sliman
+{
+    public partial class HangMan : Form
+    {
+        
+        
+        public HangMan()
+        {
+            InitializeComponent();
+        }
+        // some variables to set the numbers missed and make the words be counted properly
+        string word = "";
+        List<Label> Labels = new List<Label>();
+        int AmountMissed = 0;
+        // makes the body parts of Hangman set as variables so they can appear as they are drawn.
+        enum BodyParts
+        {
+            Head,
+            Left_Eye,
+            Right_Eye,
+            Mouth,
+            Body,
+            Left_Arm,
+            Right_Arm,
+            Left_Leg,
+            Right_Leg,
+        }
+        // draws the gallows everytime the the form is activated...@@@It Is Kinda Buggy@@@
+        void DrawGallows()
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Black, 10);
+            g.DrawLine(p, new Point(130, 218), new Point(130, 5));
+            g.DrawLine(p, new Point(135, 5), new Point(65, 5));
+            g.DrawLine(p, new Point(60, 0), new Point(60, 50));
+
+            g.Dispose(); 
+            
+        }
+        // Draws the body parts based on the set coordinates.
+        void DrawBodyParts(BodyParts Bp)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.DarkViolet, 2);
+            if (Bp == BodyParts.Head)
+            {
+                g.DrawEllipse(p, 40, 50, 40, 40);
+            }
+            else if (Bp == BodyParts.Left_Eye)
+            {
+                SolidBrush s = new SolidBrush(Color.Chartreuse);
+                g.FillEllipse(s, 50, 60, 5, 5);
+            }
+            else if (Bp == BodyParts.Right_Eye)
+            {
+                SolidBrush s = new SolidBrush(Color.Chartreuse);
+                g.FillEllipse(s, 63, 60, 5, 5);
+            }
+            else if (Bp == BodyParts.Mouth)
+            {
+                SolidBrush s = new SolidBrush(Color.DarkViolet);
+                g.DrawArc(p, 50, 60, 20, 20, 45, 90);
+            }
+            else if (Bp == BodyParts.Body)
+            {
+                g.DrawLine(p, new Point(60, 90), new Point(60, 170));
+            }
+            else if (Bp == BodyParts.Left_Arm)
+            {
+                g.DrawLine(p, new Point(60, 100), new Point(30, 85));
+            }
+            else if (Bp == BodyParts.Right_Arm)
+            {
+                g.DrawLine(p, new Point(60, 100), new Point(90, 85));
+            }
+            else if (Bp == BodyParts.Left_Leg)
+            {
+                g.DrawLine(p, new Point(60, 170), new Point(30, 190));
+            }
+            else if (Bp == BodyParts.Right_Leg)
+            {
+                g.DrawLine(p, new Point(60, 170), new Point(90, 190));
+            }
+
+            g.Dispose();
+        }
+        // makes labels appear on the form as simple __ marks that hold the value of the random word.
+        void MakeLabels()
+        {
+            word = RandomWord();
+            char[] chars = word.ToArray();
+            int space = 400 / chars.Length;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                Labels.Add(new Label());
+                Labels[i].Location = new Point((i * space) + 10, 80);
+                Labels[i].Text = "_";
+                Labels[i].Parent = groupBox2;
+                Labels[i].BringToFront();
+                Labels[i].CreateControl();
+
+            }
+            lblWordLength.Text = "Word Length: " + (chars.Length).ToString();
+        }
+        // generates a random word everytime from the list of words from the site below vvvvvvv.
+        String RandomWord()
+        {
+            WebClient wc = new WebClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            string wordlist = wc.DownloadString("https://raw.githubusercontent.com/Tom25/Hangman/master/wordlist.txt");
+            string[] words = wordlist.Split('\n');
+            Random random = new Random();
+            wc.Dispose();
+            return words[random.Next(0, words.Length)];
+            
+        }
+// makes the labels and gallows appear everytime the form is drawn.
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            DrawGallows();
+            MakeLabels();
+        }
+        // when the letter butten is clicked it checks to see if the letter is part of the word. if it is it shows on the form, and if its not the user is shown an added
+        // body part also the missed letter shows up under the missed letters label. also this button displays an error if the users attempts to submit anything other
+        // then a letter. if the user wins a messageBix is displayed saying they have won and the game restarts, and if the user loses then the game shows them the missed 
+        // word and restarts.
+        private void btnLetter_Click(object sender, EventArgs e)
+        {
+           try
+            { char letter = txtLetter.Text.ToLower().ToCharArray()[0];
+            
+                if (!char.IsLetter(letter))
+                {
+                    MessageBox.Show("you can only submit letters!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (word.Contains(letter))
+                {
+                    char[] letters = word.ToCharArray();
+                    for (int i = 0; i < letters.Length; i++)
+                    {
+                        if (letters[i] == letter)
+                            Labels[i].Text = letter.ToString();
+                    }
+                    foreach (Label l in Labels)
+                        if (l.Text == "_") return;
+                    MessageBox.Show("You Have Won!!!", "Congrats");
+                    reset();
+                }
+                else
+                {
+                    MessageBox.Show("The letter you guessed is not in the word.");
+                    lblMissed.Text += " " + letter.ToString() + ",";
+                    DrawBodyParts((BodyParts)AmountMissed);
+                    AmountMissed++;
+                    if (AmountMissed == 9)
+                    {
+                        MessageBox.Show("Sorry, you lose. The word Was " + word);
+                        reset();
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("you can only submit letters!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+// creats the value to reset the game.
+        void reset()
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(panel1.BackColor);
+            RandomWord();
+            MakeLabels();
+            DrawGallows();
+            lblMissed.Text = "Missed: ";
+            txtLetter.Text = "";
+        }
+// lets the user enter the whole word if they can and if they have all the letters it will display a messagebox to show them if they won or lost.
+        private void btnWord_Click(object sender, EventArgs e)
+        {
+            if (txtWord.Text == word)
+            {
+                MessageBox.Show("You Have Won!!!", "Congrats");
+                reset();
+            }
+            else
+            {
+                MessageBox.Show("The word you guessed is wrong, sorry!");
+                DrawBodyParts((BodyParts)AmountMissed);
+                AmountMissed++;
+                if (AmountMissed == 9)
+                {
+                    MessageBox.Show("Sorry, you lose. The word Was " + word);
+                    reset();
+                    
+                }
+            }
+        }
+// i added a clear button that sets the focus back to the letter box.
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtLetter.Text = "";
+            txtWord.Text = "";
+            txtLetter.Focus();
+        }
+// i added a reset button because sometimes the game is alittle buggy and certain features do not work right. ie. the gallows not being drawn. the player never losing. 
+        // no body parts being shown. ect.
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            reset();
+            
+        }
+// closes the form.
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            Close();
+        }
+
+      
+    }
+}
